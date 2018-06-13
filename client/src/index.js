c998b700f165a6ebba5b99f03e8fda364f5eb09c
Melissa Kepler <mmkepler@yahoo.com>
2018-06-13 18:12:28 -0400

    pakage.json update The package now tells heroku to build the project. I also updated the .gitignore to ignore the build files and deleted the build folder I had.

--- client/src/index.js
+++ client/src/index.js
@@ -4,4 +4,3 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
